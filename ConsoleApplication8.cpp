be0126d4d6c6ab3c7ef827a7d00e4b1a8c8750f9
abdulaziz <116705391+Azizkubanychbek@users.noreply.github.com>
2023-03-17 23:21:02 +0600

    Update ConsoleApplication8.cpp

--- ConsoleApplication8.cpp
+++ ConsoleApplication8.cpp
@@ -1,48 +1,86 @@
 #include <iostream>
-class Triangle {
+class Figure {
 public:
-	Triangle() {}
-	Triangle(int a,int b, int c, int A, int B,int C) {
-		
-	
+	Figure(){}
+	virtual void print_info() const {
+		std::cout << "Name : Figure\n";
+
 		std::cout << "Sides : a = " << a << " b = " << b << " c = " << c << std::endl;
-		std::cout << "Corners : A = " << A << " B = " << b << " C = " << C << std::endl;
+		std::cout << "Corners : A = " << A << " B = " << C << " C = " << C << std::endl;
 		std::cout << "\n";
 	}
 protected:
 
-		int a, b, c;
-		int A, B, C;
-
+	int a, b, c;
+	int A, B, C;
 };
-class Quadrate {
+class Triangle:public Figure {
+public:
+	Triangle() {}
+	Triangle(int a, int b, int c, int A, int B, int C) {
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->A = A;
+		this->B = B;
+		this->C = C;
+
+	}
+	 void print_info() const override{
+		 std::cout << "Name : Triangle\n";
+		 std::cout << "Sides : a = " << a << " b = " << b << " c = " << c << std::endl;
+		 std::cout << "Corners : A = " << A << " B = " << B << " C = " << C << std::endl;
+		 std::cout << "\n";
+	}
+	
+};
+class Quadrate :public Figure {
 public:
 	Quadrate() {}
-	Quadrate(int a, int b, int c,int d, int A, int B, int C,int D) {
-		std::cout << "Sides : a = " << a << " b = " << b << " c = " << c <<" d = "<<d<< std::endl;
-		std::cout << "Corners : A = " << A << " A = " << b << " C = " << C <<" D = "<<D<< std::endl;
-		std::cout << "\n\n";
+	Quadrate(int a, int b, int c, int d, int A, int B, int C, int D) {
+		
+		
+	}
+	void print_info() const override{
+		std::cout << "Name : Quadrate\n";
+
+		std::cout << "Sides : a = " << a << " b = " << b << " c = " << c << " d = " << d << std::endl;
+		std::cout << "Corners : A = " << A << " A = " << B << " C = " << C << " D = " << D << std::endl;
+		std::cout << "\n";
 	}
 protected:
-	int a, b, c, d;
-	int A, B, C, D;
+	int d;
+	int D;
 
 };
 class Prtr :public Triangle {
 public:
-	//int get_c() { return C; }
-	Prtr(int a, int b, int c, int A, int B):Triangle(a, b, c, A, B, C = 90)
+	
+	Prtr(int a, int b, int c, int A, int B) :Triangle(a, b, c, A, B, C = 90)
 	{
+		
 	}
-//private:
-	//int C = 90;
+	void print_info() const override {
+		std::cout << "Name : Prtr\n";
+		std::cout << "Sides : a = " << a << " b = " << b << " c = " << c  << std::endl;
+		std::cout << "Corners : A = " << A << " A = " << B << " C = " << C  << std::endl;
+		std::cout << "\n";
+	}
+	
 };
 class Rbtr :public Triangle {
 public:
-	
+
 
 	Rbtr(int a, int b, int A, int B) :Triangle(a, b, c = a, A, B, C = A)
 	{
+	
+	}
+	void print_info() const override {
+		std::cout << "Name : Rbtr\n";
+		std::cout << "Sides : a = " << a << " b = " << b << " c = " << c <<  std::endl;
+		std::cout << "Corners : A = " << A << " A = " << B << " C = " << C << std::endl;
+		std::cout << "\n";
 	}
 
 };
@@ -50,51 +88,81 @@ class Rtr :public Triangle {
 public:
 
 
-	Rtr(int a) :Triangle(a, a, a,60,60,60)
+	Rtr(int a) :Triangle(a, a, a, 60, 60, 60)
 	{
+	}
+	void print_info() const override {
+		std::cout << "Name : Rtr\n";
+		std::cout << "Sides : a = " << a << " b = " << b << " c = " << c << std::endl;
+		std::cout << "Corners : A = " << A << " A = " << B << " C = " << C << std::endl;
+		std::cout << "\n";
 	}
 
 };
 class Prmu :public Quadrate {
 public:
-	Prmu(int a, int b) :Quadrate(a, b, a,b, 90, 90, 90, 90) {
+	Prmu(int a, int b) :Quadrate(a, b, a, b, 90, 90, 90, 90) {
 
+	}
+	void print_info() const override {
+		std::cout << "Name : Prmu\n";
+		std::cout << "Sides : a = " << a << " b = " << b << " c = " << c << " d = " << d << std::endl;
+		std::cout << "Corners : A = " << A << " A = " << B << " C = " << C << " D = " << D << std::endl;
+		std::cout << "\n";
 	}
 };
 class Kv :public Quadrate {
 public:
-	Kv(int a):Quadrate(a,a,a,a,90,90,90,90){}
+	Kv(int a) :Quadrate(a, a, a, a, 90, 90, 90, 90) {}
+	void print_info() const override {
+		std::cout << "Name : Kv\n";
+		std::cout << "Sides : a = " << a << " b = " << b << " c = " << c << " d = " << d << std::endl;
+		std::cout << "Corners : A = " << A << " A = " << B << " C = " << C << " D = " << D << std::endl;
+		std::cout << "\n";
+	}
 };
 class Par :public Quadrate {
 public:
-	Par(int a, int b, int A, int B):Quadrate(a,b,a,b,A,B,A,B){}
+	Par(int a, int b, int A, int B) :Quadrate(a, b, a, b, A, B, A, B) {}
+	void print_info() const override {
+		std::cout << "Name : Par\n";
+		std::cout << "Sides : a = " << a << " b = " << b << " c = " << c << " d = " << d << std::endl;
+		std::cout << "Corners : A = " << A << " A = " << B << " C = " << C << " D = " << D << std::endl;
+		std::cout << "\n";
+	}
 };
 class Rm :public Quadrate {
 public:
-	Rm(int a, int A,int B):Quadrate(a,a,a,a,A,B,A,B){}
+	Rm(int a, int A, int B) :Quadrate(a, a, a, a, A, B, A, B) {}
+	void print_info() const override {
+		std::cout << "Name : Rm\n";
+		std::cout << "Sides : a = " << a << " b = " << a << " c = " << c << " a = " << d << std::endl;
+		std::cout << "Corners : A = " << A << " A = " << B << " C = " << A << " D = " << B << std::endl;
+		std::cout << "\n";
+	}
 };
+void print_info(const Figure* figure) {
+	figure->print_info();
+}
 
 int main()
 {
-	std::cout << "triangle:\n";
-	Triangle triangle(10,20,30,40,50,60);
-	std::cout << "Prtr: \n";
+	Triangle triangle(10, 20, 30, 40, 50, 60);
 	Prtr prtr(29, 39, 59, 21, 33);
-	std::cout << "Rbtr: \n";
-	Rbtr rbtr(10, 20, 20, 10);
-	std::cout << "Rtr: \n";
+	//Rbtr rbtr(10, 20, 20, 10);
 	Rtr rtr(15);
-	std::cout << "Quadrat: \n";
 	Quadrate quadrate(10, 20, 30, 40, 50, 60, 70, 80);
-	std::cout << "Prmu: \n";
 	Prmu prmu(10, 20);
-	std::cout << "Kv: \n";
 	Kv kv(20);
-	std::cout << "Par: \n";
 	Par par(10, 12, 60, 30);
-	std::cout << "Rm: \n";
-	Rm rm(10, 30, 60);
+	//Rm rm(10, 30, 60);
+	Rm* rm = new Rm(10, 30, 60);
+	Rbtr* rbtr = new Rbtr(10, 12, 60, 30);
+	print_info(rbtr);
+	print_info(rm);
+	delete rm;
+	delete rbtr;
 
 
-	
+
 }
