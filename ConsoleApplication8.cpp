e71c92f20b51b050968e9dfbb98c588eab129dfe
abdulaziz <116705391+Azizkubanychbek@users.noreply.github.com>
2023-03-18 16:11:09 +0600

    Update ConsoleApplication8.cpp

--- ConsoleApplication8.cpp
+++ ConsoleApplication8.cpp
@@ -38,6 +38,15 @@ class Quadrate :public Figure {
 public:
 	Quadrate() {}
 	Quadrate(int a, int b, int c, int d, int A, int B, int C, int D) {
+			this->a = a;
+		this->b = b;
+		this->c = c;
+		this->A = A;
+		this->B = B;
+		this->C = C;
+		this->D = D;
+		this->d = d;
+
 		
 		
 	}
